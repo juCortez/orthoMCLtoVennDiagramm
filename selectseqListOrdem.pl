269c035bf8228dedcf0d908d778197b98515db7e
Juliano Mimura Cortez <juCortez@users.noreply.github.com>
2018-05-04 16:05:54 -0300

    Update selectseqListOrdem.pl

--- selectseqListOrdem.pl
+++ selectseqListOrdem.pl
@@ -7,30 +7,28 @@ use strict;
 use warnings;
 use autodie; # die if problem reading or writing a file
 use Statistics::R;
-
 my @list=("Serg","Tbru","Lmajor");
 my $R = Statistics::R->new();
 $R->send("library(VennDiagramm);");
 $R->send('library("venneuler");');
 foreach my $i (@list){
 my @entries;
+my $file = "out".$i
 open FILE1, $ARGV[0] or die $!; #lista
 open FILE2, $ARGV[1] or die $!; #prank fasta file
+open (wFile, '>>$file') or die $!;
 while(<FILE1>){
         my @prefix = split(":",$_);
         push @entries, $prefix[0] if /\b$i\b/
-        ##if (/\b$i\b/){print $prefix[0],"\n";}##tambem funciona
+        if (/\b$i\b/){print wFile $prefix[0],"\n";}##tambem funciona
         }close FILE1;
 
 while(<FILE2>){
 	my @prefix = split(":",$_);
         push @entries, $prefix[0] if /\b$i\b/
-        ##if (/\b$i\b/){print $prefix[0],"\n";}##tambem funciona	
+        if (/\b$i\b/){print wFile $prefix[0],"\n";}##tambem funciona	
 	}close FILE2;
 	
-$R->set( 'x', \@entries );
-my $x = $R->get('x');
-print $x;
 }
 
 ##------------------------------------------------------------------------
