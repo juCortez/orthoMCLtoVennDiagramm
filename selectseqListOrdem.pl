44f52c7aef320f566bf83dd67fa2f82f75ec1d86
Juliano Mimura Cortez <juCortez@users.noreply.github.com>
2018-05-04 11:23:13 -0300

    Update selectseqListOrdem.pl

--- selectseqListOrdem.pl
+++ selectseqListOrdem.pl
@@ -10,43 +10,29 @@ use Statistics::R;
 
 my @list=("Serg","Tbru","Lmajor");
 my $R = Statistics::R->new();
-
-foreach $i (@list){
+$R->send("library(VennDiagramm);");
+foreach my $i (@list){
 my @entries;
 open FILE1, $ARGV[0] or die $!; #lista
 open FILE2, $ARGV[1] or die $!; #prank fasta file
 while(<FILE1>){
         my @prefix = split(":",$_);
         push @entries, $prefix[0] if /\b$i\b/
-	##if (/\b$i\b/){print $prefix[0],"\n";}##tambem funciona
-	
+        ##if (/\b$i\b/){print $prefix[0],"\n";}##tambem funciona
         }close FILE1;
-	$R->set( 'x', \@entries );
-	print $R->get('x');
+
 while(<FILE2>){
-		
+	my @prefix = split(":",$_);
+        push @entries, $prefix[0] if /\b$i\b/
+        ##if (/\b$i\b/){print $prefix[0],"\n";}##tambem funciona	
 	}close FILE2;
+	
+$R->set( 'x', \@entries );
+my $x = $R->get('x');
+print $x;
 }
+
 ##------------------------------------------------------------------------
-my @list=("Serg","Tbru","Lmajor");
 
-foreach my $i (@list){
-open FILE1, $ARGV[0] or die $!; #lista
-print $i,"\n";
-while(<FILE1>){
-        my @prefix = split(":",$_);
-        print $prefix[0],"\n" if /\b$i\b/
-	##if (/\b$i\b/){print $prefix[0],"\n";}##tambem funciona
-        }close FILE1;
-}
-use Statistics::R;
 
-my @numbers = ( 1 .. 10 );
-
-my $R = Statistics::R->new();
-$R->set( 'x', \@numbers );
-$R->run( q`x = x ^ 2` );
-my $squares = $R->get('x');
-
-print "@$squares";
 
