30bfe351602fb289d98942a36fee539886463910
Juliano Mimura Cortez <juCortez@users.noreply.github.com>
2018-05-04 12:07:13 -0300

    Update selectseqListOrdem.pl

--- selectseqListOrdem.pl
+++ selectseqListOrdem.pl
@@ -11,6 +11,7 @@ use Statistics::R;
 my @list=("Serg","Tbru","Lmajor");
 my $R = Statistics::R->new();
 $R->send("library(VennDiagramm);");
+$R->send('library("venneuler");');
 foreach my $i (@list){
 my @entries;
 open FILE1, $ARGV[0] or die $!; #lista
@@ -34,5 +35,6 @@ print $x;
 
 ##------------------------------------------------------------------------
 
-
+vd <- venneuler(c(A=0.3, B=0.3, C=1.1, "A&B"=0.1, "A&C"=0.2, "B&C"=0.1 ,"A&B&C"=0.1))
+plot(vd)
 
