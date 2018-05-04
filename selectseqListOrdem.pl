43bf301b9401a95e846a38b4bfad4ec6b151a02d
Juliano Mimura Cortez <juCortez@users.noreply.github.com>
2018-05-04 09:47:39 -0300

    Update selectseqListOrdem.pl

--- selectseqListOrdem.pl
+++ selectseqListOrdem.pl
@@ -6,14 +6,23 @@
 use strict;
 use warnings;
 use autodie; # die if problem reading or writing a file
+use Statistics::R;
+
 my @list=("Serg","Tbru","Lmajor");
+my $R = Statistics::R->new();
 
 foreach $i (@list){
+my @entries;
 open FILE1, $ARGV[0] or die $!; #lista
 open FILE2, $ARGV[1] or die $!; #prank fasta file
 while(<FILE1>){
-        print $_ if /\b$i\b/
+        my @prefix = split(":",$_);
+        push @entries, $prefix[0] if /\b$i\b/
+	##if (/\b$i\b/){print $prefix[0],"\n";}##tambem funciona
+	
         }close FILE1;
+	$R->set( 'x', \@entries );
+	print $R->get('x');
 while(<FILE2>){
 		
 	}close FILE2;
