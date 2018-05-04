64e966ac2113a6d92b7600f6cfaaa1152568cb68
Juliano Mimura Cortez <juCortez@users.noreply.github.com>
2018-05-04 19:24:05 -0300

    Update selectseqListOrdem.pl

--- selectseqListOrdem.pl
+++ selectseqListOrdem.pl
@@ -7,32 +7,40 @@ use strict;
 use warnings;
 use autodie; # die if problem reading or writing a file
 use Statistics::R;
-my @list=("Serg","Tbru","Lmajor");
+my @list=("Serg","Tbru","Lmajor","Tgrayi");
 my $R = Statistics::R->new();
-$R->send("library(VennDiagramm);");
+my $temp;
+my $counter = 0;
+$R->send("library(gplots);");
 $R->send('library("venneuler");');
 foreach my $i (@list){
+	$counter++;
 my @entries;
-my $file = "out".$i
+my $file = "out".$i;
 open FILE1, $ARGV[0] or die $!; #lista
 open FILE2, $ARGV[1] or die $!; #prank fasta file
-open (wFile, '>>$file') or die $!;
+open (wFile, ">> $file") or die $!;
 while(<FILE1>){
         my @prefix = split(":",$_);
-        push @entries, $prefix[0] if /\b$i\b/
+        push @entries, $prefix[0] if /\b$i\b/;
         if (/\b$i\b/){print wFile $prefix[0],"\n";}##tambem funciona
         }close FILE1;
 
 while(<FILE2>){
 	my @prefix = split(":",$_);
-        push @entries, $prefix[0] if /\b$i\b/
+        push @entries, $prefix[0] if /\b$i\b/;
         if (/\b$i\b/){print wFile $prefix[0],"\n";}##tambem funciona	
 	}close FILE2;
-	
+	if ($counter<scalar(@list)){$temp .= $i." = ".$i.",";}
+	else{$temp .= $i." = ".$i;}
+	$R->set($i, \@entries);
+
 }
+my $z="v<-venn(list(".$temp."))";
+$R->run($z);
 
+# 	$R->set($i, \@entries);
+# }
+# $R->run(q'v<-venn(list(A=Serg,B=Tbru,C=Lmajor))');
+##########q'v<-venn(list(Serg = Serg,Tbru = Tbru,Lmajor = Lmajor))'
 ##------------------------------------------------------------------------
-
-vd <- venneuler(c(A=0.3, B=0.3, C=1.1, "A&B"=0.1, "A&C"=0.2, "B&C"=0.1 ,"A&B&C"=0.1))
-plot(vd)
-
